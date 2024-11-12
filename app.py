6fd057d2e64b0bcb5b7d28e8567bb56c84b377fc
2realzoo <realzoojin@gmail.com>
2024-11-12 14:43:53 +0900

    feat: 광고 생성 연결, logging 기능 추가

--- app.py
+++ app.py
@@ -1,124 +1,30 @@
 import os
 from uuid import uuid4
 import base64
-from AnalyzeMeeting.lda_model import TopicModel
-from AnalyzeMeeting.stt import stt
-from AnalyzeMeeting.text_organize import remove_stopwords, tokenize_text
-from dotenv import load_dotenv
 from fastapi import FastAPI, File, Form, HTTPException, UploadFile, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
 from utils.upload_s3 import upload_file_to_s3
-from AnalyzeMeeting.embedding_vector_model import EmbeddingVectorModel
+# from comfyApi.epicrealizm_img2img_api import make_image
+# from comfyApi.cogvideo_img2vid_api import make_video
+from comfyUIApi import make_advertise
+import logging
 
-# 환경변수 로드
-load_dotenv()
+logging.basicConfig(
+    filename='app.log',        # 로그 파일 이름
+    filemode='a',               # 'a'는 기존 파일에 추가, 'w'는 파일을 덮어쓰기
+    level=logging.INFO,         # 로그 레벨 설정
+    encoding='utf-8-sig',
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
 
 # FastAPI와 템플릿 설정
-app = FastAPI()
-templates = Jinja2Templates(directory="./templates")
-
-# embedding_model 생성
-embedding_vector_model = EmbeddingVectorModel()
-
-# 토큰 저장소
-tokens = {}
-
-# 텍스트 답변 처리 엔드포인트
-class TextResponse(BaseModel):
-    surveyQuestion: str
-    textResponse: str
-    userId: int
-    meetingId: int
-    corpId: int
-
-@app.post("/submit-text-response")
-async def submit_text_response(response: TextResponse):
-    if response.corpId not in tokens:
-        tokens[response.corpId] = {response.meetingId: []}
-    tokens[response.corpId][response.meetingId].extend(remove_stopwords(tokenize_text(response.textResponse)))
-    return {"result": f"{response.textResponse}"}
-
-# 음성 답변 처리 엔드포인트 (음성 파일을 STT로 변환)
-class VoiceResponse(BaseModel):
-    surveyQuestion: str 
-    voiceResponse: str
-    userId: int 
-    meetingId: int 
-    corpId: int 
-@app.post("/submit-voice-response")
-async def submit_voice_response(response: VoiceResponse):
-    try:
-        voice_data = base64.b64decode(response.voiceResponse)
-        text_response = stt(voice_data)
-        if response.corpId not in tokens:
-            tokens[response.corpId] = {response.meetingId: []}
-        tokens[response.corpId][response.meetingId].extend(remove_stopwords(tokenize_text(text_response)))
-        return {"result": f"{text_response}"}
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=f'Error processing voice response: {e}')
-
-# 설문 선택 답변 처리 엔드포인트
-class ChoiceResponse(BaseModel):
-    surveyQuestion: str
-    choiceResponse: str
-    userId: int
-    meetingId: int
-    corpId: int
-    
-@app.post("/submit-choice-response")
-async def submit_choice_response(response: ChoiceResponse):
-    # 선택형 설문 데이터를 저장
-    return {"result": "Choice response received"}
-
-# 최종 분석 처리 엔드포인트
-class AnalyzeResponse(BaseModel):
-    corpId: int
-    meetingId: int
-
-@app.post("/analyze-responses")
-async def analyze_responses(response: AnalyzeResponse):
-    try:
-        topic_model = TopicModel(tokens[response.corpId][response.meetingId], response.corpId, response.meetingId)
-        json_result = topic_model.make_lda_json()
-        
-        embedding_vector_model.make_embeddings(tokens[response.corpId][response.meetingId], response.corpId, response.meetingId)
-        embedding_vector_model.make_checkpoint()
-        embedding_vector_model.run_tensorboard('0.0.0.0', '7779')
-        return json_result
-    except KeyError:
-        if response.corpId not in tokens:
-            raise HTTPException(status_code=404, detail="corpId에 해당하는 데이터가 존재하지 않습니다.")
-        elif response.meetingId not in tokens[response.corpId]:
-            raise HTTPException(status_code=404, detail='meetingId에 해당하는 데이터가 존재하지 않습니다.')
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=f"error: {e}")
-
-# FastAPI로 HTML 파일을 서빙
-@app.get("/index.html", response_class=HTMLResponse)
-async def serve_html():
-    try:
-        with open("./index.html", "r") as f:
-            html_content = f.read()
-        return HTMLResponse(content=html_content, status_code=200)
-    except FileNotFoundError:
-        raise HTTPException(status_code=404, detail="HTML file not found.")
-
-# LDA 시각화 처리 엔드포인트
-@app.post("/lda-visualization")
-async def lda_visualization(request: Request, response: AnalyzeResponse):
-    try:
-        topic_model = TopicModel(tokens[response.corpId][response.meetingId], response.corpId, response.meetingId)
-        json_result = topic_model.make_lda_json()
-
-        # 템플릿에 데이터를 전달하여 렌더링
-        return templates.TemplateResponse("lda_visualization.html", {
-            "request": request,
-            "lda_data": json_result
-        })
-    except KeyError:
-        raise HTTPException(status_code=404, detail="No data found for the given corpId and meetingId.")
+app = FastAPI(    
+    title="광고 생성 API 문서",
+    description="영상 광고 생성 기능",
+    version="1.0.0"
+    )
 
 # Video Ad 생성 요청에 필요한 이미지 파일과 프롬프트 받기
 @app.post("/generate-videoad")
@@ -126,29 +32,39 @@ async def generate_videoad(
     image: UploadFile = File(...),
     prompt: str = Form(...),
     corpId: int = Form(...),
-    type: str = Form(...)
-):
+    ratioType: str = Form(...)
+):   
+    # try:
+    logging.info(f"/generate-videoad : {image, prompt, corpId, ratioType}")
     AWS_DEFAULT_REGION = os.getenv("AWS_DEFAULT_REGION")
-    try:
-        # 이미지 파일 저장 경로 설정
-        image_path = f"/tmp/{image.filename}"
+    # 이미지 파일 저장 경로 설정
+    image_path = f"/home/metaai1/jinjoo_work/unicorn/ComfyUI/input/{str(uuid4())}"
 
-        # 이미지 파일을 저장
-        with open(image_path, "wb") as buffer:
-            buffer.write(await image.read())
+    # 이미지 파일을 저장
+    with open(image_path, "wb") as buffer:
+        buffer.write(await image.read())
 
+    file_urls = []
+    generated_video_name = str(uuid4()) + '.mp4'
+    if ratioType == "가로형":
+        video_paths = make_advertise(image_path, prompt, generated_video_name, False)
+    elif ratioType == "세로형":
+        video_paths = make_advertise(image_path, prompt, generated_video_name, True)
+
+    for video_path in video_paths:
         # S3 버킷에 업로드
         bucket = 'jurassic-park'
-        key = str(uuid4()) + '.jpg'
-        file_url = f"https://{bucket}.s3.{AWS_DEFAULT_REGION}.amazonaws.com/{key}"
-        await upload_file_to_s3(image_path, key)
+        file_url = f"https://{bucket}.s3.{AWS_DEFAULT_REGION}.amazonaws.com/{generated_video_name}"
+        await upload_file_to_s3(video_path, key)
+        file_urls.append(file_url)
+        
+    # 실행 결과 확인
+    return {"result": file_urls}
 
-        # 실행 결과 확인
-        return {"result": file_url}
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=f'Error: {e}')
+    # except Exception as e:
+    #     raise HTTPException(status_code=400, detail=f'Error: {e}')
 
-@app.post("generate-background")
+@app.post("/generate-background")
 async def generate_background(
     image: UploadFile = File(...),
     prompt: str = Form(...),
